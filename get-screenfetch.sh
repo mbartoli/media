0df8fdf49745489f174335bd095cc52ec9eaba8a
mbartoli <bartolimichael@gmail.com>
2015-02-21 07:54:17 -0800

    adding screenfetch installation script

---
+++ get-screenfetch.sh
@@ -0,0 +1,6 @@
+mkdir ~/screenfetch
+cd ~/screenfetch
+wget -O screenfetch 'https://raw.github.com/KittyKatt/screenFetch/master/screenfetch-dev'
+chmod +x screenfetch
+sudo cp screenfetch /usr/local/bin
+rm -r ~/screenfetch
